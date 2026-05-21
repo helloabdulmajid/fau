407efe16de294ef77aae8ed7e6103764134f8047
Abdul Majid <helloabdulmajid@gmail.com>
2026-05-21 17:04:57 +0530

    chore: initialize Spring Boot backend project

---
+++ src/main/java/in/abdulmajid/cardiq/CardiqApplication.java
@@ -0,0 +1,13 @@
+package in.abdulmajid.cardiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardiqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardiqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/abdulmajid/cardiq/CardiqApplicationTests.java
@@ -0,0 +1,13 @@
+package in.abdulmajid.cardiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardiqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
